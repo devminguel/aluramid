928f634bcef97440c4de2cb651f1eb6591069f2b
Luis Miguel Lima Rodrigues <163193019+devminguel@users.noreply.github.com>
2024-06-17 11:41:54 -0300

    Alura Mid
    Bateria Digital

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,54 @@
+function tocaSom(seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+
+
+    if (elemento && elemento.localName === null && elemento.localName === 'audio') {
+        
+            elemento.play();
+        }
+        else{
+             console.log('Elemento não encontrado ou seletor inválido');
+        }
+    
+
+    }
+
+
+document.querySelectorAll('.tecla');
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//para
+for ( let contador = 0 ; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+
+    const idAudio = `#som_${instrumento}`;
+
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+   tecla.onkeydown = function (evento) {
+
+   
+    if(evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');  
+    }
+
+        
+   }
+
+   tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+   }
+}
+
+
